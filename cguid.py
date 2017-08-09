e3f59248ceb7b84e7912c9f45415db6346c73567
Doug Ilijev <doilij@microsoft.com>
2017-08-09 14:39:37 -0700

    Make cguid.py work in Python27

--- cguid.py
+++ cguid.py
@@ -4,12 +4,12 @@ cguid.py <GUID>
 Where GUID is formatted as XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
 Where each X is a hex character /[\\dA-F]/i
 
-http://icanhasguid.com/ can generate GUIDs in this form.
+http://icanhasguid.com/ or uuidgen.exe (from the Windows 8+ SDK) can generate GUIDs in this form.
 
 Outputs the GUID in the form of a C GUID struct literal.
 
-Works with Python3 (tested with 3.6.0).
-AFAIK, does not work with Python2, despite the print_function import (tested with 2.7.13).
+Works with Python27 (tested with 2.7.13)
+Works with Python3 (tested with 3.6.0)
 """
 
 from __future__ import print_function
@@ -22,22 +22,27 @@ GUID_RE = re.compile(r'.*([\dA-F]{8})-([\dA-F]{4})-([\dA-F]{4})-([\dA-F]{4})-([\
 def main():
     """ main """
 
+    error = False
     if len(sys.argv) >= 2:
         guid = sys.argv[1]
-        print(guid)
         matches = GUID_RE.match(guid)
         if matches:
-            print("Found match: " + guid)
+            print(guid)
             groups = matches.groups()
             group_3_pairs = re.match(r"(..)(..)", groups[3]).groups()
             group_4_pairs = re.match(r"(..)(..)(..)(..)(..)(..)", groups[4]).groups()
+            pairs = list(group_3_pairs)
+            pairs.extend(list(group_4_pairs))
             formatted = ("{{ 0x{}, 0x{}, 0x{}, " \
                 + "{{ 0x{}, 0x{}, 0x{}, 0x{}, 0x{}, 0x{}, 0x{}, 0x{} }} }};") \
-                .format(groups[0], groups[1], groups[2], *group_3_pairs, *group_4_pairs)
+                .format(groups[0], groups[1], groups[2], *pairs)
             print(formatted)
         else:
-            print("Not a valid GUID")
+            error = True
     else:
-        print("Please provide a GUID formatted as XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX")
+        error = True
+
+    if error:
+        print("Please provide a GUID formatted as XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX where each X is a Hex Digit (/[0-9A-F]/i)")
 
 main()
